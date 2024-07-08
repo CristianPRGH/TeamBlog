24d1f059d4ff5eeade78fc58551ccf9b078561d8
Paula-LL <plizelucea@gmail.com>
2024-07-08 10:58:15 +0200

    cargar vista

---
+++ vista/LoginVista.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head> 
+<body>
+   <form method="post" action="LoginVista.php">
+        <input type="text" name="username" placeholder="Introduce your name"><br>
+        <input type="password" name="password" placeholder="Introduce your password"><br>
+        <input type="submit" name="send" value="Login">
+   </form> 
+</body>
+</html>
