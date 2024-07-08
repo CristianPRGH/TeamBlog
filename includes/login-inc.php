7ff755933964654f9b41978ec1a596b01a419866
CristianPRGH <cpr.1992@gmail.com>
2024-07-08 13:16:53 +0200

    update

--- includes/login-inc.php
+++ includes/login-inc.php
@@ -1,10 +1,11 @@
 <?php
 
 require "autoload.controllers.php";
-require "autoload.model.php";
+require "autoload.models.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["send"]))
 {
+    // print_r($_POST);
     $username = $_POST["username"];
     $password = $_POST["password"];
 
