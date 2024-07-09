04a33be2c0fb7d61c378be5245807773e296cd6d
jaumemonasterio <jmonasteriosella@gmail.com>
2024-07-09 12:49:20 +0200

    updates

--- model/Post.php
+++ model/Post.php
@@ -6,7 +6,7 @@ class Post extends coneccio{
     protected function addPosts($title, $text, $img){
 
         $error= 0;
-        $stmt = $this-> conectar()->prepare("INSERT INTO posts (`titulo`, `text`, 'img ') VALUES (?, ? , ?);");
+        $stmt = $this-> conectar()->prepare("INSERT INTO posts (`titulo`, `text`, `img`) VALUES (?, ? , ?);");
 
         if(!$stmt->execute(array($title, $text, $img))){
             $error=1;
